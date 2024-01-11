2cd69255ac8f0463595a8e4de3c33aa4fd5973a2
Karol <karolkaraszewski@gmail.com>
2024-01-11 19:51:46 +0100

    update 2

--- src/Main.java
+++ src/Main.java
@@ -1,3 +1,6 @@
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.List;
 import java.util.Scanner;
 
 public class Main {
@@ -15,7 +18,22 @@ public class Main {
         // String word = scanner.nextLine();
         // System.out.println(obiektZadanie3.countWords(word));
 
-           Zadanie4 obiectTask4Module3 = new Zadanie4();
+            List<Student> listOfStudents = new ArrayList<>();
+            Student student1 = new Student(20, 180, "John", "Doe");
+            Student student2 = new Student(21, 179,"Roger", "Doe");
+            Student student3 = new Student(22, 178, "Steven", "Doe");
+            Student student4 = new Student(23, 177, "Mike", "Doe");
+            Student student5 = new Student(24, 176, "Tony", "Doe");
+            listOfStudents.add(student1);
+            listOfStudents.add(student2);
+            listOfStudents.add(student3);
+            listOfStudents.add(student4);
+            listOfStudents.add(student5);
+
+            System.out.println("Before Sorting : " + listOfStudents);
+            Collections.sort(listOfStudents);
+            System.out.println("After Sorting : " + listOfStudents);
+        }
+
 
     }
-}
---
+++ src/Student.java
@@ -0,0 +1,51 @@
+public class Student implements Comparable<Student> {
+    private int age;
+    private int height;
+    private String name;
+    private String lastName;
+
+    public Student(int age, int height, String name, String lastName) {
+        this.age = age;
+        this.height = height;
+        this.name = name;
+        this.lastName = lastName;
+    }
+
+    public int getAge() {
+        return age;
+    }
+
+    public int getHeight() {
+        return height;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public String getLastName() {
+        return lastName;
+    }
+
+    public void setAge(int age) {
+        this.age = age;
+    }
+
+    public void setHeight(int height) {
+        this.height = height;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public void setLastName(String lastName) {
+        this.lastName = lastName;
+    }
+
+
+    @Override
+    public int compareTo(Student otherStudent) {
+        return Integer.compare(getAge(), otherStudent.getAge());
+    }
+}
